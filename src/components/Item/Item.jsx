9617fd85f1c2e9a477386182689272fb59b201dc
JakeDer29 <108018683+Yakov29@users.noreply.github.com>
2024-11-28 09:48:25 +0200

    update

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,4 +1,9 @@
 import "./Item.css";
-export const Item = () => {
-  return <li className="item"></li>;
+export const Item = ({fruits}) => {
+  console.log(fruits)
+  return <li className="item">
+    <h3 className="fruit__name">{fruits.name}</h3>
+    <img className="fruit__photo" src={fruits.photo} alt="photo" />
+    <p className="fruit__price">{fruits.price}</p>
+  </li>;
 };
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,11 +1,14 @@
 import "./List.css";
 import { Item } from "../Item/Item";
 export const List = ({fruitsData}) => {
+  console.log(fruitsData)
     // Дестрыктырызували об'єкт
     // const {fruitsData} = data
   return <ul className="list">
     {
-        
+        fruitsData.map((data) => {
+          return <Item fruits={data}/>
+        })
     }
   </ul>;
 };
