2b73753016090b67706bece778c5c15472a30d54
JakeDer29 <108018683+Yakov29@users.noreply.github.com>
2024-11-28 10:07:25 +0200

    props

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Title } from "./components/Title/Title";
 import { List } from "./components/List/List";
 
+
 const fruits = [
   {
     name: "apple",
--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,9 +1,23 @@
 import "./Item.css";
+import PropTypes from 'prop-types';
+
+
 export const Item = ({fruits}) => {
+  const {name, photo, price} = fruits
   console.log(fruits)
   return <li className="item">
-    <h3 className="fruit__name">{fruits.name}</h3>
-    <img className="fruit__photo" src={fruits.photo} alt="photo" />
-    <p className="fruit__price">{fruits.price}</p>
+    <h3 className="fruit__name">{name}</h3>
+    <img className="fruit__photo" src={photo} alt="photo" />
+    <p className="fruit__price">{price}</p>
   </li>;
 };
+
+
+
+Item.propTypes = {
+  // You can declare that a prop is a specific JS primitive. By default, these
+  // are all optional.
+  name: PropTypes.string,
+  photo: PropTypes.string,
+  price: PropTypes.number,
+}
